fb30fbc8391da4563dd8a008c29604da3052fe28
Alina Perez <alina.perez@duke.edu>
2020-07-14 10:11:55 -0400

    radio table working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RadioComponent } from './components/radio/radio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RadioComponent
   ],
   imports: [
     BrowserModule,
@@ -17,6 +21,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/radio/radio.component.ts
+++ src/app/components/radio/radio.component.ts
@@ -1,19 +1,37 @@
 import { Component } from '@angular/core';
-import { Song } from '';
+import { Song } from 'src/app/song'; 
 
 const SONG_DATA: Song[] = [
     {
     title: "the sheep goes BAA",
     artist: "Jacob Rubin",
     albumName:"The Musical Alphabet",
-}
+    trackID: "1",
+    albumCoverURL: "https://www.google.com/url?sa=i&url=https%3A%2F%2Farts.duke.edu%2Fnews%2Fmaking-music-matter%2F&psig=AOvVaw3iMhI5zMR4OirXZqEevwgm&ust=1594821774644000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMiNv570zOoCFQAAAAAdAAAAABAD", 
+    timeAdded: "",
+    votes: 1
+    },
+    {
+    title: "this ain't DECAF", 
+    artist: "Jacob Rubin",
+    albumName: "The Musical Alphabet",
+    trackID: "2",
+    albumCoverURL: "https://www.google.com/url?sa=i&url=https%3A%2F%2Farts.duke.edu%2Fnews%2Fmaking-music-matter%2F&psig=AOvVaw3iMhI5zMR4OirXZqEevwgm&ust=1594821774644000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMiNv570zOoCFQAAAAAdAAAAABAD", 
+    timeAdded: "",
+    votes: 1
+    },
+     
 ]
+
+
 
 @Component({
     templateUrl: './radio.component.html',
-    styleUrls: ['./radio.component.css']  
+    styleUrls: ['./radio.component.css'],
+    selector: 'radio'
 })
 
 export class RadioComponent{
-    
-}; 
+    displayedColumns: string[ ]= [ "albumCoverURL", "Title", "Artist", "Album", "votes"];
+    dataSource = SONG_DATA;
+}
