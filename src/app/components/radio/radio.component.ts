44e2148a84d8ba2a97fcbeb5ef5fdd69ebae11c6
emmynk35 <nichareekhawsam-ang2021@u.northwestern.edu>
2020-07-15 12:07:18 -0500

    added playback

--- src/app/components/radio/radio.component.ts
+++ src/app/components/radio/radio.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, Inject } from '@angular/core';
 import { Song } from 'src/app/song'; 
 import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RadioAddSongComponent } from './radio-addSong.component';
 import { MatDialog } from '@angular/material/dialog';
 import { MatDialogActions }  from '@angular/material/dialog';
+import { inject } from '@angular/core/testing';
 //import { SpotifyService } from 'src/app/spotify.services';
 
 const SONG_DATA: Song[] = [
@@ -132,11 +133,56 @@ export class RadioComponent{
     song = new FormControl('');    
 
     //add spotify service
-    constructor(private router:Router, public dialog: MatDialog){}
+    constructor(private router:Router, public dialog: MatDialog, private renderer: Renderer2) {}
     
     displayedColumnsSearch: string[]= ["actions", "Title", "Artist"];
     dataSourceSearch = SONG_DATA;
 
+    ngOnInit() {
+        //install spotify webplayer SDK
+        const s = this.renderer.createElement('script');
+        s.onload = this.loadSDKScript.bind(this);
+        s.type = 'text/javascript';
+        s.src = 'https://sdk.scdn.co/spotify-player.js';
+        s.text = '';
+        this.renderer.appendChild(document.body, s);
+    }
+
+    loadSDKScript() {
+        const s = this.renderer.createElement('script');
+        s.text = `
+            window.onSpotifyWebPlaybackSDKReady = () => {
+                const token = '[My Spotify Web API access token]';
+                const player = new Spotify.Player({
+                name: 'Web Playback SDK Quick Start Player',
+                getOAuthToken: cb => { cb(token); }
+                });
+        
+                // Error handling
+                player.addListener('initialization_error', ({ message }) => { console.error(message); });
+                player.addListener('authentication_error', ({ message }) => { console.error(message); });
+                player.addListener('account_error', ({ message }) => { console.error(message); });
+                player.addListener('playback_error', ({ message }) => { console.error(message); });
+        
+                // Playback status updates
+                player.addListener('player_state_changed', state => { console.log(state); });
+        
+                // Ready
+                player.addListener('ready', ({ device_id }) => {
+                console.log('Ready with Device ID', device_id);
+                });
+        
+                // Not Ready
+                player.addListener('not_ready', ({ device_id }) => {
+                console.log('Device ID has gone offline', device_id);
+                });
+        
+                // Connect to the player!
+                player.connect();
+            };
+        `;
+        this.renderer.appendChild(document.body, s);
+    }
 
     search(): void {
         console.log(this.song.value);
